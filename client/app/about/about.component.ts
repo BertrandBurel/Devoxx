c05f2577788fbdd71b5b8214ad12f9f05c5fc556
Ludovic Lanchon <ludovic.lanchon@gmail.com>
2018-05-03 12:59:45 +0200

    test gsap + update db access

--- client/app/about/about.component.ts
+++ client/app/about/about.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
+import { TweenMax, TimelineMax, TimelineLite } from 'gsap';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
 })
 export class AboutComponent {
 
-  constructor() { }
+
+  // @ViewChild('animeObject') animationObject: ElementRef;
+
+  constructor() {
+    // this.layerAnimation();
+  }
+
+  // layerAnimation() {
+  //   const anime: TimelineMax = new TimelineMax();
+
+  //   anime.from(this.animationObject.nativeElement, 1, { x: -200 });
+
+  //   return anime;
+  // }
 
 }
